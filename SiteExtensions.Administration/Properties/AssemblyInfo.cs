57352b4d3aae7ee72b10cd6f5cba3cf4332a2644
Pranav K <prkrishn@hotmail.com>
2013-10-10 16:18:40 -0700

    Initial comit

---
+++ SiteExtensions.Administration/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SiteExtensions.Administration")]
+[assembly: AssemblyDescription("")]
+
